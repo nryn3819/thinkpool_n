0d61eb5079735d502bad46fc2f9c97ec81148f96
jhLee <nryn6563@kakao.com>
2021-06-25 15:54:21 +0900

    공유하기 레이어 수정

--- js/main.js
+++ js/main.js
@@ -143,6 +143,12 @@ $(function () {
             $('.share_pop').fadeOut();
         });
     });
+    $('.btn-share2').on('click', function(){
+        $('.share_pop2').fadeIn();
+        $('.share_pop2').find('.ly_close').children('button').on('click', function(){
+            $('.share_pop2').fadeOut();
+        });
+    });
 
     //운영배심원
     $('.btn-operjr').on('click', function () {
