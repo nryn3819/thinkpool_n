6c568d7a1c31535ed44a241f6448fc023ae77501
jhLee <nryn6563@kakao.com>
2021-07-29 15:03:43 +0900

    라씨매매신호 하단 테이블
    ? 마우스 오버시 안내문 적용
    70.html
    signal.js
    변경

--- js/main.js
+++ js/main.js
@@ -268,6 +268,28 @@ $(function () {
         });
     });
 
+    $('.overGuide').popover({
+        trigger: 'manual',
+        html: true,
+        placement : 'bottom',
+        container : '.signal_condition'
+    }).on('mouseenter', function () {
+        var _this = this;
+
+        $(this).popover('show');
+        $('.popover').on('mouseleave', function () {
+            $(_this).popover('hide');
+        });
+    }).on('mouseleave', function () {
+        var _this = this;
+
+        setTimeout(function () {
+            if (!$('.popover:hover').length) {
+                $(_this).popover('hide');
+            }
+        });
+    });
+
     //report
 
     $(function(){
