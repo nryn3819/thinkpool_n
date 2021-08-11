3fcdba9244d8a2d79bb4fad2e9f3ca6aa89b4c5e
jhLee <nryn6563@kakao.com>
2021-08-11 16:07:01 +0900

    AI알고리즘 1차 업로드

--- js/main.js
+++ js/main.js
@@ -340,6 +340,28 @@ $(function () {
     });
     // 팝업내 팝업
 
+    $('.o-top').popover({
+        trigger: 'manual',
+        html: true,
+        placement : 'top',
+        container : 'body'
+    }).on('mouseenter', function () {
+        var _this = this;
+
+        $(this).popover('show');
+        $('.popover').on('mouseleave', function () {
+            $(_this).popover('hide');
+        });
+    }).on('mouseleave', function () {
+        var _this = this;
+
+        setTimeout(function () {
+            if (!$('.popover:hover').length) {
+                $(_this).popover('hide');
+            }
+        });
+    });
+
     //module
 
 
--- js/report.js
+++ js/report.js
@@ -24,3 +24,25 @@ $(function(){
         $(_this).popover('hide');
     });
 });
+
+$('.o-top').popover({
+    trigger: 'manual',
+    html: true,
+    placement : 'top',
+    container : 'body'
+}).on('mouseenter', function () {
+    var _this = this;
+
+    $(this).popover('show');
+    $('.popover').on('mouseleave', function () {
+        $(_this).popover('hide');
+    });
+}).on('mouseleave', function () {
+    var _this = this;
+
+    setTimeout(function () {
+        if (!$('.popover:hover').length) {
+            $(_this).popover('hide');
+        }
+    });
+});
